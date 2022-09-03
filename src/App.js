2ac25be251880ec3fcb435cf5d8a62f2cf457d76
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-09-03 22:28:27 +0300

    Implemented dynamic fetching by myself :) Yeeeey

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+
 const App = () => {
+  const [age, setAge] = useState(null);
+  const [name, setName] = useState('');
+  const fetchData = (name) => {
+    Axios.get(`https://api.agify.io/?name=${name}`)
+      .then((res) => setAge(res.data.age));
+  }
+
   return (
     <div className="App">
-      
+      <input onChange={(e) => {setName(e.target.value)}} placeholder='Name...' />
+      <button onClick={() => {fetchData(name)}}>Predict age</button>
+      <h1>Predicted age: {age}</h1>
     </div>
   );
 }
