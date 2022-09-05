94728e14e1ddeff411eab0610e918d816c68756c
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-09-06 00:50:34 +0300

    React Query is added

--- src/App.js
+++ src/App.js
@@ -4,30 +4,39 @@ import PredictAge from './components/PredictAge';
 import Excuses from './components/Excuses';
 import Home from './components/Home';
 import Profile from './components/Profile';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
 export const AppContext = createContext();
 
 const App = () => {
+  const client = new QueryClient({defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true
+    }
+  }});
+
   const [userName, setUserName] = useState('Ilia');
 
   return (
     <div className="App">
-      <AppContext.Provider value={{userName, setUserName}}>
-        <BrowserRouter>
-          <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/profile'>Profile</Link>
-            <Link to='/predict-age'>Predict age</Link>
-            <Link to='/excuses'>Excuses</Link>
-          </nav>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/predict-age' element={<PredictAge />} />
-            <Route path='/excuses' element={<Excuses />} />
-          </Routes>
-        </BrowserRouter>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{userName, setUserName}}>
+          <BrowserRouter>
+            <nav>
+              <Link to='/'>Home</Link>
+              <Link to='/profile'>Profile</Link>
+              {/* <Link to='/predict-age'>Predict age</Link> */}
+              {/* <Link to='/excuses'>Excuses</Link> */}
+            </nav>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/predict-age' element={<PredictAge />} />
+              <Route path='/excuses' element={<Excuses />} />
+            </Routes>
+          </BrowserRouter>
+        </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,28 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../App';
+import { useQuery } from '@tanstack/react-query';
+import Axios from 'axios';
 
 const Home = () => {
   const {userName} = useContext(AppContext);
+  const {data, isLoading, isError, refetch} = useQuery(['cat'], async () => {
+    return Axios.get('https://catfact.ninja/fact').then((res) => res.data)
+  });
+
+  if (isLoading) {
+    return <h1>Loading...</h1>
+  }
+
+  if (isError) {
+    return <h1>There was an error</h1>
+  }
+
   return (
     <div>
       <h1>Home page</h1>
       <h3>User name is {userName}</h3>
+      <h1>{data?.fact}</h1>
+      <button onClick={refetch}>Update</button>
     </div>
   )
 }
